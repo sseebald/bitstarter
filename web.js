d91eac5d07b610b565e834ec80d99062d5b84cc4
Spencer Seebald <sseebald@gmail.com>
2013-07-14 06:39:09 +0000

    Update web.js

--- web.js
+++ web.js
@@ -14,9 +14,11 @@ var app = express.createServer(express.logger());
 
 fs.readFile('/home/ubuntu/bitstarter/index.html', function (err, data) {
   if (err) throw err;
-  app.get('/', function(request, response) {
+console.log(data);});
+
+
+app.get('/', function(request, response) {
   response.send("Test");});
-});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
