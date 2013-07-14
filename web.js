23b61e75edff7d70a8a9e42bc841cdf19bb63747
Spencer Seebald <sseebald@gmail.com>
2013-07-14 05:45:34 +0000

    Update web.js

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buffer = new Buffer(fs.readFile('/home/ubuntu/bitstarter/index.html',"utf-8");
+var buffer = new Buffer(fs.readFile('/home/ubuntu/bitstarter/index.html'),"utf-8");
 
 app.get('/', function(request, response) {
   response.send('Test');
