5a09aa765f43ca027b9d60152b514d3c050a38a5
Spencer Seebald <sseebald@gmail.com>
2013-07-14 05:48:42 +0000

    Update web.js

--- web.js
+++ web.js
@@ -2,10 +2,15 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buffer = new Buffer(16);
+
+buffer.write("Hello","utf-8");
+
+
 //var buffer = new Buffer(fs.readFile('/home/ubuntu/bitstarter/index.html'),"utf-8");
 
 app.get('/', function(request, response) {
-  response.send('Test');
+  response.send(buffer);
 });
 
 var port = process.env.PORT || 5000;
