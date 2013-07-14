1704ff20ca305562dd86a9e602bbb52617340a78
Spencer Seebald <sseebald@gmail.com>
2013-07-14 18:45:03 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,9 +2,9 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-//var buffer = new Buffer(fs.readFileSync('/home/ubuntu/bitstarter/index.html', function (err, data) {
-//  if (err) throw err; 
-//  console.log(data)}));
+var buffer = new Buffer(fs.readFileSync('/home/ubuntu/bitstarter/index.html', function (err, data) {
+  if (err) throw err; 
+  console.log(data)}));
 
 app.get('/', function(request, response) {
   response.send("Test");});
