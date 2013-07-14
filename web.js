84b62eff8f84ef539b48191ab50a859f669ed24c
Spencer Seebald <sseebald@gmail.com>
2013-07-14 19:19:33 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -13,7 +13,8 @@ buffer = fs.readFileSync('/home/ubuntu/bitstarter/index.html');
 //buffer.toString();
 
 app.get('/', function(request, response) {
-  response.send("Test");});
+  response.send('Test');
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
