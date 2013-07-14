3bdffa1068524fee7ea0fee88887e9debfbeb4d4
Spencer Seebald <sseebald@gmail.com>
2013-07-14 06:33:27 +0000

    Update web.js

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buffer = new Buffer(fs.readFileSync('/home/ubuntu/bitstarter/index.html');
+//var buffer = new Buffer(fs.readFileSync('/home/ubuntu/bitstarter/index.html');
 
 //fs.readFileSync('/etc/passwd');
 
@@ -12,9 +12,19 @@ var buffer = new Buffer(fs.readFileSync('/home/ubuntu/bitstarter/index.html');
 
 //var buffer = new Buffer(fs.readFile('/home/ubuntu/bitstarter/index.html'),"utf-8");
 
-app.get('/', function(request, response) {
+fs.readFile('/etc/passwd', function (err, data) {
+  if (err) throw err;
+  app.get('/', function(request, response) {
   response.send(buffer.toString());
 });
+ 
+  //console.log(data);
+});
+
+
+//app.get('/', function(request, response) {
+//  response.send(buffer.toString());
+//});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
