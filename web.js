e7517a54765aefcc146cd04c8ec0123bd219f579
Spencer Seebald <sseebald@gmail.com>
2013-07-14 19:56:36 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,18 +1,14 @@
-//var fs = require('fs');
+var fs = require('fs');
 
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buffer = new Buffer(fs.readFileSync("./index.html"));
-
 app.get('/', function(request, response) {
-//  response.send(fs.readFileSync("./index.html").toString());
-  response.send(buffer.toString());
+  response.send(fs.readFileSync("./index.html").toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
