74002fde0ebd1d097179aca6c5210fa24eb1d762
Spencer Seebald <sseebald@gmail.com>
2013-07-14 06:14:24 +0000

    Update web.js

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buffer = new Buffer(16);
+var buffer = new Buffer(255);
 
 //fs.readFileSync('/etc/passwd');
 
